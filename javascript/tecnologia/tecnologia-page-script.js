092a987026382a183a36a3a03d535644b15be306
Mauricio Marques <134658196+MauricioDevBR@users.noreply.github.com>
2023-07-06 00:33:15 -0300

    Adição de novo dado a array e correção de bugs

--- javascript/tecnologia/tecnologia-page-script.js
+++ javascript/tecnologia/tecnologia-page-script.js
@@ -100,27 +100,11 @@ const arrayCarrossel = [
     },
 
     {
-        linkNotices: 'https://www.gov.br/governodigital/pt-br/vlibras/',
-        cardImg: 'https://www.gov.br/governodigital/pt-br/vlibras/imagens/avatar.png',
-        altImgCard: 'Avatares do VLibras: Guga, Ícaro e Hozana',
-        tittleCard: 'VLibras - Tradução automática para tornar a Web mais acessível.',
-        descriptionCard: 'As aplicações da Suíte VLibras permitem que pessoas surdas acessem conteúdo multimídia em sua língua natural de comunicação, o que contribui para com a acessibilidade de computadores, dispositivos móveis e páginas Web.'
-    },
-
-    {
-        linkNotices: 'https://olhardigital.com.br/2020/11/03/noticias/pesquisadores-dizem-que-o-google-usa-recaptcha-para-captar-dados/',
-        cardImg: 'https://proxy.olhardigital.com.br/wp-content/uploads/2020/11/20201103064551.jpg',
-        altImgCard: 'Imagem da verificação: "Não sou um robô"',
-        tittleCard: 'Pesquisadores dizem que o Google usa reCAPTCHA para coletar dados',
-        descriptionCard: 'Segundo analistas, ferramenta anti-spam tem sido usada para criar rastros de navegação e direcionar anúncio aos usuários.'
-    },
-
-    {
-        linkNotices: 'https://tecnoblog.net/noticias/2023/06/22/para-ficar-com-activision-microsoft-admite-que-xbox-perdeu-guerra-dos-consoles/',
-        cardImg: 'https://files.tecnoblog.net/wp-content/uploads/2022/04/xbox-x_capa-2_tb-1060x596.png',
-        altImgCard: 'Imagem do console Microsoft Xbox',
-        tittleCard: 'Para ficar com Activision, Microsoft admite que Xbox perdeu guerra dos consoles.',
-        descriptionCard: 'Em um documento enviado à corte, a Microsoft revela que, desde que foi lançado, em 2001, “o console Xbox ficou consistentemente com a terceira posição entre as três concorrentes, atrás de PlayStation e Nintendo nas vendas”.',
+        linkNotices: 'https://g1.globo.com/tecnologia/noticia/2023/07/05/instagram-threads-rival-twitter.ghtml',
+        cardImg: 'https://s2-g1.glbimg.com/XvJgv46mktFspeaOXTWubWa87Mg=/0x0:6000x3375/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/y/6/jynisCQqywFngyjBH5Jg/aplicativo-threads.png',
+        altImgCard: 'Imagens da nova rede social: "Threads"',
+        tittleCard: 'Instagram lança Threads, rede social para rivalizar com o Twitter.',
+        descriptionCard: 'Rede social foi revelada dias após o Twitter anunciar um limite para leitura de posts. Usuários poderão fazer login e encontrar amigos por meio de suas contas no Instagram.'
     },
 
     {
@@ -135,9 +119,35 @@ const arrayCarrossel = [
         linkNotices: 'https://www.oficinadanet.com.br/playstation/45086-playstation-2-fatos-curiosidades',
         cardImg: 'https://www.oficinadanet.com.br/media/post/45086/750/playstation-2.jpg',
         altImgCard: 'Imagem do console PlayStation 2',
-        tittleCard: 'PlayStation 2 completa 23 anos: relembre fatos e curiosidades',
+        tittleCard: 'PlayStation 2 completa 23 anos: relembre fatos e curiosidades.',
         descriptionCard: 'Com um design icônico e jogos inesquecíveis, como GTA San Andreas, Final Fantasy X e Metal Gear Solid 2, o PlayStation 2 tornou-se um dos consoles mais populares de todos os tempos.'
     },
+
+    {
+        linkNotices: 'https://www.gov.br/governodigital/pt-br/vlibras/',
+        cardImg: 'https://www.gov.br/governodigital/pt-br/vlibras/imagens/avatar.png',
+        altImgCard: 'Avatares do VLibras: Guga, Ícaro e Hozana',
+        tittleCard: 'VLibras - Tradução automática para tornar a Web mais acessível.',
+        descriptionCard: 'As aplicações da Suíte VLibras permitem que pessoas surdas acessem conteúdo multimídia em sua língua natural de comunicação, o que contribui para com a acessibilidade de computadores, dispositivos móveis e páginas Web.'
+    },
+
+    {
+        linkNotices: 'https://olhardigital.com.br/2020/11/03/noticias/pesquisadores-dizem-que-o-google-usa-recaptcha-para-captar-dados/',
+        cardImg: 'https://proxy.olhardigital.com.br/wp-content/uploads/2020/11/20201103064551.jpg',
+        altImgCard: 'Imagem da verificação: "Não sou um robô"',
+        tittleCard: 'Pesquisadores dizem que o Google usa reCAPTCHA para coletar dados.',
+        descriptionCard: 'Segundo analistas, ferramenta anti-spam tem sido usada para criar rastros de navegação e direcionar anúncio aos usuários.'
+    },
+
+    {
+        linkNotices: 'https://tecnoblog.net/noticias/2023/06/22/para-ficar-com-activision-microsoft-admite-que-xbox-perdeu-guerra-dos-consoles/',
+        cardImg: 'https://files.tecnoblog.net/wp-content/uploads/2022/04/xbox-x_capa-2_tb-1060x596.png',
+        altImgCard: 'Imagem do console Microsoft Xbox',
+        tittleCard: 'Para ficar com Activision, Microsoft admite que Xbox perdeu guerra dos consoles.',
+        descriptionCard: 'Em um documento enviado à corte, a Microsoft revela que, desde que foi lançado, em 2001, “o console Xbox ficou consistentemente com a terceira posição entre as três concorrentes, atrás de PlayStation e Nintendo nas vendas”.',
+    },
+
+    
 
 
 ]
