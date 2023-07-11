b689a508e0b8678cbdb3bd02c73df905c65c81ab
Mauricio Marques <134658196+MauricioDevBR@users.noreply.github.com>
2023-07-11 15:12:45 -0300

    migração de código HTML para JS, criação de array em JS com os dados da página "Sobre Nós" e adição de estilo (CSS)

--- javascript/sobre-nos/sobre-nos.js
+++ javascript/sobre-nos/sobre-nos.js
@@ -1,4 +1,4 @@
-    // Carrosel - Código de funcionamento (geral)
+// Carrosel - Código de funcionamento (adaptado para a página 'Sobre nós')
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 1,
     spaceBetween: 1,
@@ -19,14 +19,12 @@ var swiper = new Swiper(".slide-content", {
         prevEl: ".swiper-button-prev"
     },
 
-    
-
 });  
 
 
-//Arquivos de alimentação do carrosel - Cards da aba "Sobre Nós"
+//Arquivos de alimentação do mini carrosel lateral - Sobre Nós
 
-const arrayCarrossel = [
+const arrayMiniCarrossel = [
 
     {
         href: 'culinaria.html',
@@ -43,7 +41,7 @@ const arrayCarrossel = [
     },
 ]
 
-for (let index = 0; index < arrayCarrossel.length; index++) {
+for (let index = 0; index < arrayMiniCarrossel.length; index++) {
     // Criar elemento
     let carrosselScript = document.createElement('div')
 
@@ -52,8 +50,8 @@ carrosselScript.className = 'card swiper-slide'
 
 // Manipular elemento
     carrosselScript.innerHTML = `
-        <a href="${arrayCarrossel[index].href}">
-            <img class="img-card" src="${arrayCarrossel[index].cardImg}" alt="not informed">
+        <a href="${arrayMiniCarrossel[index].href}">
+            <img class="img-card" src="${arrayMiniCarrossel[index].cardImg}" alt="not informed">
         </a>
     `
 
@@ -64,4 +62,108 @@ carrosselScript.className = 'card swiper-slide'
     selecionarClasseCarrossel.appendChild(carrosselScript)
     
 }
+
+
+
+// Arquivos de texto que ficam ao lado do mini carrosel
+
+const arrayTextoSobreNos = [
+    {
+        primeiroParagrafo: 'O <a target="_blank" href="index.html"><strong class="nome-jornal-grifado">Grid da Notícia</strong></a> é um site indexador de notícias, desenvolvido por estudantes de tecnologia, que tem o objetivo de trazer as principais informações sobre: <a href="culinaria.html">Culinária</a>, <a href="espaco_sideral.html">Espaço Sideral</a> e <a href="tecnologia.html">Tecnologia</a>.',
+    },
+
+    {
+        segundoParagrafo: 'O nosso principal propósito, é o de promover um site que seja adaptável, seguro e acessível, onde vocês possam se informar das principais notícias dos temas que temos disponíveis, sem se preocupar com as "fake news", nós não tratamos nossos apoiadores como produtos, por isso, muito mais vale seriedade e credibilidade do que "clicks".',
+    },
+] 
+
+//Criar elemento 
+
+let textoSobreNos = document.createElement('div')
+
+//Manipular elemento
+
+textoSobreNos.innerHTML = `
+    <p>
+        ${arrayTextoSobreNos[0].primeiroParagrafo}
+    </p>
+
+    <br>
+
+    <p>
+        ${arrayTextoSobreNos[1].segundoParagrafo}
+    </p>
+
+`
+
+let selecionarDivTextoSobreNos = document.querySelector('.texto-sobre-nos')
+
+selecionarDivTextoSobreNos.appendChild(textoSobreNos)
+
+
+
+
+
+
+
+// Cards
+
+const arrayCardsSobreNos = [
+    {
+        img: 'https://media.tenor.com/p1gzBFA712oAAAAC/totoro-mi-vecino-totoro.gif',
+        altImg: '',
+        titleCard: 'Adaptáveis:',
+        descriptionCard: 'Somos <strong>adaptáveis</strong>, pois cada tema tem sua própria personalização e identidade, o que mostra o cuidado que temos com a experiência de navegação em nosso site.',
+    },
+
+    {
+        img: 'https://strati.com.br/wp-content/uploads/2021/12/32-topo-seguranca-da-informacao.jpg',
+        altImg: '',
+        titleCard: 'Seguro:',
+        descriptionCard: 'O Grid da Notícia é <strong>seguro</strong>, exatamente por prezar pela seriedade e credibilidade do que por "clicks". <br> <br> Até nossos parceiros publicitários passam por um rigoroso processo de aprovação, para poderem anunciar conosco, assim como, as notícias que são divulgadas em nossas páginas.',
+    },
+
+    {
+        img: 'https://tribox.com.br/wp-content/uploads/2018/01/capa-ok.jpg',
+        altImg: '',
+        titleCard: 'Acessivel:',
+        descriptionCard: 'De nada adianta apoiarmos a causa de mais <strong>acessibilidade</strong>, se não fizermos a nossa parte. <br> <br> Por isso, sentimos orgulho de contar com o plug-in de acessibilidade <strong> <a href="https://www.gov.br/governodigital/pt-br/vlibras" target="_blank" rel="noopener noreferrer">VLibras</a></strong> em nosso site.',
+    },
+
+]
+
+for (let index = 0; index < arrayCardsSobreNos.length; index++) {
+
+// Criar elemento:
+    let cardsSobreNos = document.createElement('div')
+
+// Atribuir uma classe ao elemento:
+    cardsSobreNos.className = 'card-somos'
+
+// Manipular elemento:
+    cardsSobreNos.innerHTML = `
+        <img class="img-card" src=${arrayCardsSobreNos[index].img}" alt="${arrayCardsSobreNos[index].altImg}">
+
+        <div class="card-info">
+            <h3 class="title_card">
+                ${arrayCardsSobreNos[index].titleCard}
+            </h3>
+
+            <p class="description_card">
+                ${arrayCardsSobreNos[index].descriptionCard}
+            </p>
+        </div>
+        
+    `
+
+//
+
+// Adicionar elemento:
+    let sectionCardsSobreNosSomos = document.querySelector('.cards-sobre-nos-somos')
+    sectionCardsSobreNosSomos.appendChild(cardsSobreNos)
+
+}
+
+
+
 
