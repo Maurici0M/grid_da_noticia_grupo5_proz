97920ab98a8e5d8424d8df67e955983dd9a257da
Michael <michael.s.paes4@gmail.com>
2023-07-15 19:35:44 -0300

    Espace midia changed

--- javascript/espaco_sideral/espaco_sideral-page-script.js
+++ javascript/espaco_sideral/espaco_sideral-page-script.js
@@ -1,114 +1,129 @@
 // Carrosel - Código de funcionamento
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: "auto",
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-    },
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false
+
+    660: {
+      slidesPerView: "auto",
     },
 
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        
-        660: {
-            slidesPerView: 'auto',
-        },
-
-        950: {
-            slidesPerView: 'auto',
-        }
-    }
-
-  });  
-
-
-
+    950: {
+      slidesPerView: "auto",
+    },
+  },
+});
 
 //Arquivos de alimentação do carrosel - Notícias que são mostradas
 
 const arrayCarrossel = [
+  {
+    linkNotices:
+      "https://spacetoday.com.br/inteligencia-artificial-no-estudo-do-campo-magnetico-solar/",
+    cardImg:
+      "https://spacetoday.com.br/wp-content/uploads/2023/07/ia_campo_magnetico_sol_01.jpg",
+    altImgCard: "Imagem ilustrativa do campo magnético solar",
+    tittleCard: "Inteligência Artificial No Estudo Do Campo Magnético Solar",
+    descriptionCard:
+      "Cientistas da Universidade de Graz e do Skoltech alcançaram um avanço significativo na física solar, conforme publicado na revista Nature Astronomy.",
+  },
 
-    {
-        linkNotices: 'https://www.estadao.com.br/link/discord-o-que-e-e-como-funciona-o-app-para-gamers-que-virou-foco-de-abusos-contra-jovens/',
-        cardImg:'https://t.ctcdn.com.br/OBqzxQ888c3zUJEzGrxu4jjioOo=/512x288/smart/filters:format(webp)/i525670.png',
-        altImgCard:'Logo do aplicativo Discord',
-        tittleCard: 'Discord: O que é e como funciona o app para gamers que virou foco de abusos contra jovens.',
-        descriptionCard: 'Plataforma ganhou popularidade entre os jovens por transmissões ao vivo de vídeos das partidas, mas virou mecanismo da ação de criminosos para subornar menores.'
-    },
+  {
+    linkNotices:
+      "https://spacetoday.com.br/elon-musk-fala-das-grandes-atualizacoes-do-starship/",
+    cardImg:
+      "https://spacetoday.com.br/wp-content/uploads/2023/06/starship_01.png",
+    altImgCard: "Imagem do foguete Starship",
+    tittleCard: "Elon Musk Fala Das Grandes Atualizações do Starship",
+    descriptionCard:
+      "A SpaceX precisará de mais seis semanas ou mais para concluir a implementação de centenas de mudanças em seu foguete Super Heavy-Starship e na gigantesca plataforma de lançamento do Texas.",
+  },
 
-    {
-        linkNotices: 'https://g1.globo.com/tecnologia/noticia/2023/07/05/instagram-threads-rival-twitter.ghtml',
-        cardImg: 'https://s2-g1.glbimg.com/XvJgv46mktFspeaOXTWubWa87Mg=/0x0:6000x3375/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/y/6/jynisCQqywFngyjBH5Jg/aplicativo-threads.png',
-        altImgCard: 'Imagens da nova rede social: "Threads"',
-        tittleCard: 'Instagram lança Threads, rede social para rivalizar com o Twitter.',
-        descriptionCard: 'Rede social foi revelada dias após o Twitter anunciar um limite para leitura de posts. Usuários poderão fazer login e encontrar amigos por meio de suas contas no Instagram.'
-    },
+  {
+    linkNotices:
+      "https://spacetoday.com.br/o-legado-do-helicoptero-ingenuity-em-marte/",
+    cardImg:
+      "https://spacetoday.com.br/wp-content/uploads/2023/05/ingenuity.jpg",
+    altImgCard: "Imagem do Helicóptero Ingenuity",
+    tittleCard: "O Legado do Helicóptero Ingenuity Em Marte",
+    descriptionCard:
+      "Em 19 de abril de 2021, um pequeno helicóptero chamado Ingenuity fez história ao realizar o primeiro voo motorizado e controlado em outro planeta.",
+  },
 
-    {
-        linkNotices: 'https://www.hardware.com.br/noticias/2023-06/marvel-e-criticada-por-usar-inteligencia-artificial-para-criar-abertura-de-serie.html',
-        cardImg: 'https://www.hardware.com.br/wp-content/uploads/2023/06/WhatsApp-Image-2023-06-22-at-11.11.20.jpeg',
-        altImgCard: 'Imagem da nova série Marvel: Invasão Secreta',
-        tittleCard: 'Marvel é criticada por usar inteligência artificial para criar abertura de série.',
-        descriptionCard: 'A controvérsia surgiu após a revelação de que a Marvel usou IA para criar a sequência de abertura de sua nova série, “Invasão Secreta”.'
-    },
+  {
+    linkNotices:
+      "https://spacetoday.com.br/james-webb-estuda-em-detalhe-um-mini-netuno/",
+    cardImg:
+      "https://spacetoday.com.br/wp-content/uploads/2023/06/1-hazyminineptune-16.jpg",
+    altImgCard: "Imagem do Mini-Netuno ",
+    tittleCard: "James Webb Estuda Em Detalhe Um Mini-Netuno",
+    descriptionCard:
+      "Uma equipe de cientistas obtém uma nova visão sobre a atmosfera de um “mini-Netuno”, uma classe de planeta comum na galáxia, mas sobre a qual pouco se sabe.",
+  },
 
-    {
-        linkNotices: 'https://www.oficinadanet.com.br/playstation/45086-playstation-2-fatos-curiosidades',
-        cardImg: 'https://www.oficinadanet.com.br/media/post/45086/750/playstation-2.jpg',
-        altImgCard: 'Imagem do console PlayStation 2',
-        tittleCard: 'PlayStation 2 completa 23 anos: relembre fatos e curiosidades.',
-        descriptionCard: 'Com um design icônico e jogos inesquecíveis, como GTA San Andreas, Final Fantasy X e Metal Gear Solid 2, o PlayStation 2 tornou-se um dos consoles mais populares de todos os tempos.'
-    },
+  {
+    linkNotices:
+      "https://spacetoday.com.br/a-procura-por-bacterias-e-entendimentos-sobre-o-manto-terrestre-na-iss/",
+    cardImg:
+      "https://spacetoday.com.br/wp-content/uploads/2016/12/iss050e011020-1024x682.jpg",
+    altImgCard: "Cientista na ISS",
+    tittleCard:
+      "A procura por bactérias e entendimentos sobre o manto terrestre na ISS",
+    descriptionCard:
+      "Os trabalhos da nova tripulação da ISS/NASA estão mais interessantes do que nunca! Com o dobro de membros tripulantes, as pesquisas científicas ganharam um up!",
+  },
 
-    {
-        linkNotices: 'https://www.gov.br/governodigital/pt-br/vlibras/',
-        cardImg: 'https://www.gov.br/governodigital/pt-br/vlibras/imagens/avatar.png',
-        altImgCard: 'Avatares do VLibras: Guga, Ícaro e Hozana',
-        tittleCard: 'VLibras - Tradução automática para tornar a Web mais acessível.',
-        descriptionCard: 'As aplicações da Suíte VLibras permitem que pessoas surdas acessem conteúdo multimídia em sua língua natural de comunicação, o que contribui para com a acessibilidade de computadores, dispositivos móveis e páginas Web.'
-    },
+  {
+    linkNotices:
+      "https://revistagalileu.globo.com/ciencia/espaco/noticia/2023/07/nasa-divulga-foto-deslumbrante-de-lagoa-no-rs-diretamente-do-espaco.ghtml",
+    cardImg:
+      "https://s2-galileu.glbimg.com/RclyAxzTmJyrstSzCizMTSSTfJs=/0x0:1600x1064/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_fde5cd494fb04473a83fa5fd57ad4542/internal_photos/bs/2023/h/V/4RIemKTaqKU1LBkV3fOw/whatsapp-image-2023-07-14-at-10.18.36.jpeg",
+    altImgCard: "Imagem da lagoa no RS",
+    tittleCard:
+      "Nasa divulga foto deslumbrante de lagoa no RS diretamente do espaço",
+    descriptionCard:
+      "Imagem feita pelo ex-astronauta Jack Fischer a partir da Estação Espacial Internacional (ISS) mostra a beleza da Lagoa dos Barros, no litoral gaúcho.",
+  },
 
-    {
-        linkNotices: 'https://olhardigital.com.br/2020/11/03/noticias/pesquisadores-dizem-que-o-google-usa-recaptcha-para-captar-dados/',
-        cardImg: 'https://proxy.olhardigital.com.br/wp-content/uploads/2020/11/20201103064551.jpg',
-        altImgCard: 'Imagem da verificação: "Não sou um robô"',
-        tittleCard: 'Pesquisadores dizem que o Google usa reCAPTCHA para coletar dados.',
-        descriptionCard: 'Segundo analistas, ferramenta anti-spam tem sido usada para criar rastros de navegação e direcionar anúncio aos usuários.'
-    },
-
-    {
-        linkNotices: 'https://tecnoblog.net/noticias/2023/06/22/para-ficar-com-activision-microsoft-admite-que-xbox-perdeu-guerra-dos-consoles/',
-        cardImg: 'https://files.tecnoblog.net/wp-content/uploads/2022/04/xbox-x_capa-2_tb-1060x596.png',
-        altImgCard: 'Imagem do console Microsoft Xbox',
-        tittleCard: 'Para ficar com Activision, Microsoft admite que Xbox perdeu guerra dos consoles.',
-        descriptionCard: 'Em um documento enviado à corte, a Microsoft revela que, desde que foi lançado, em 2001, “o console Xbox ficou consistentemente com a terceira posição entre as três concorrentes, atrás de PlayStation e Nintendo nas vendas”.',
-    },
-
-    
-
-
-]
+  {
+    linkNotices:
+      "https://revistagalileu.globo.com/ciencia/espaco/noticia/2023/07/entenda-a-foto-comemorativa-do-primeiro-ano-de-atividade-do-james-webb.ghtml",
+    cardImg:
+      "https://s2-galileu.glbimg.com/HsNsFP9yQHqpGx87m0WOKyxrIm4=/0x0:1280x1199/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_fde5cd494fb04473a83fa5fd57ad4542/internal_photos/bs/2023/p/o/YgiPfLRHOm0m7L3lJquw/weic2316a.jpg",
+    altImgCard: "Imagem da nebulosa",
+    tittleCard:
+      "Entenda a foto comemorativa do primeiro ano de atividade do James Webb",
+    descriptionCard:
+      "Nova imagem apresenta a região de formação estelar mais próxima da Terra, no complexo de nuvens Rho Ophiuchi.",
+  },
+];
 
 for (let index = 0; index < arrayCarrossel.length; index++) {
-    // Criar elemento
-    let carrosselScript = document.createElement('div')
+  // Criar elemento
+  let carrosselScript = document.createElement("div");
 
-// Atribuir uma classe ao elemento (caso necessário)
-carrosselScript.className = 'card swiper-slide'
+  // Atribuir uma classe ao elemento (caso necessário)
+  carrosselScript.className = "card swiper-slide";
 
-// Manipular elemento
-    carrosselScript.innerHTML = `
+  // Manipular elemento
+  carrosselScript.innerHTML = `
         <a class="link-notices" target="_blank" href="${arrayCarrossel[index].linkNotices}">
             <div class="image-content">
                 <span class="overlay">
@@ -127,21 +142,14 @@ carrosselScript.className = 'card swiper-slide'
             </div>
         </a>
 
-    `
+    `;
 
-// Adicionar na página
+  // Adicionar na página
 
-    let selecionarClasseCarrossel = document.querySelector('.card-wrapper')
+  let selecionarClasseCarrossel = document.querySelector(".card-wrapper");
 
-    selecionarClasseCarrossel.appendChild(carrosselScript)
-    
+  selecionarClasseCarrossel.appendChild(carrosselScript);
 }
-
-
-
-
-
-
 
 // Script original do Swiper "Freemode" (desativado)
 
