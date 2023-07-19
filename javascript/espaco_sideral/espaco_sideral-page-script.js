79ddedde950ab478280b0565a8435ade5782eb8c
Michael <michael.s.paes4@gmail.com>
2023-07-19 19:53:13 -0300

    resizing elements on the sidebar

--- javascript/espaco_sideral/espaco_sideral-page-script.js
+++ javascript/espaco_sideral/espaco_sideral-page-script.js
@@ -170,7 +170,7 @@ for (let index = 0; index < arrayCarrossel.length; index++) {
   }); */
 
 function openNav() {
-  document.getElementById("side_nav").style.width = "300px";
+  document.getElementById("side_nav").style.width = "25%";
 }
 function closeNav() {
   document.getElementById("side_nav").style.width = "0";
