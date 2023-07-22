5590f3eaac8b25e173a0f735bc832084ac0cb610
Michael <michael.s.paes4@gmail.com>
2023-07-22 00:02:13 -0300

    fix

--- javascript/login.js
+++ javascript/login.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const loginForm = document.querySelector("login");
-  const creatAccountForm = document.querySelector("createAccount");
+  const loginForm = document.querySelector("#login");
+  const creatAccountForm = document.querySelector("#createAccount");
 
-  document.querySelector("linkCreateAccount").addEventListener("click", (e) => {
+  document
+    .querySelector("#linkCreateAccount")
+    .addEventListener("click", (e) => {
+      e.preventDefault();
+      loginForm.classList.add("form_hidden");
+      creatAccountForm.classList.remove("form_hidden");
+    });
+
+  document.querySelector("#linkLogin").addEventListener("click", (e) => {
     e.preventDefault();
-    loginForm.classList.add("form_hidden");
-    creatAccountForm.classList.remove("form_hidden");
-  });
-
-  document.querySelector("linkLogin").addEventListener("click", (e) => {
-    e.preventDefault;
     loginForm.classList.remove("form_hidden");
     creatAccountForm.classList.add("form_hidden");
   });
