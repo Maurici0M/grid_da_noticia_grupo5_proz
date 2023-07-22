3a8486bd431724e381aa1cb3cd04e236c8b7d3fc
Michael <michael.s.paes4@gmail.com>
2023-07-22 20:03:21 -0300

    form username validation, plus login form button adjusted

--- javascript/login.js
+++ javascript/login.js
@@ -1,3 +1,25 @@
+function setFormMessage(formElement, type, message) {
+  const messageElement = formElement.querySelector(".form_message");
+
+  messageElement.textContent = message;
+  messageElement.classList.remove("form_message_success", "form_message_error");
+  messageElement.classList.add(`form_message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+  inputElement.classList.add("form_input_error");
+  inputElement.parentElement.querySelector(
+    ".form_input-error-message"
+  ).textContent = message;
+}
+
+function clearInputError(inputElement) {
+  inputElement.classList.remove("form_Input_error");
+  inputElement.parentElement.querySelector(
+    ".form_input-error-message"
+  ).textContent = "";
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const creatAccountForm = document.querySelector("#createAccount");
@@ -14,5 +36,30 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     loginForm.classList.remove("form_hidden");
     creatAccountForm.classList.add("form_hidden");
+  });
+
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    setFormMessage(loginForm, "error", "Nome de usuário ou senha incorretos");
+  });
+
+  document.querySelectorAll(".form_input").forEach((inputElement) => {
+    inputElement.addEventListener("blur", (e) => {
+      if (
+        e.target.id === "signupUsername" &&
+        e.target.value.length > 0 &&
+        e.target.value.length < 7
+      ) {
+        setInputError(
+          inputElement,
+          "O nome de usuário deve possuir ao menos 7 caracteres"
+        );
+      }
+    });
+
+    inputElement.addEventListener("input", (e) => {
+      clearInputError(inputElement);
+    });
   });
 });
