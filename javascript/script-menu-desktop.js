01f8cdc2f575689ae29194b8efddcd90c0fba85b
Mauricio Marques <134658196+MauricioDevBR@users.noreply.github.com>
2023-07-24 00:31:30 -0300

    New image added and improvements applied to the search bar (stable) - test version only on the tecnologia page.

---
+++ javascript/script-menu-desktop.js
@@ -0,0 +1,50 @@
+function openNav() {
+    document.getElementById("side_nav").style.width = "25%";
+    // document.getElementById("#").style.marginLeft = "#";
+  
+    const navbar = document.querySelector('.navbar-header')
+    navbar.style.transition = 'all 0.9s ease'
+  
+    const body = document.querySelector('body')
+    body.style.marginLeft = '21rem'
+    body.style.transition = 'all 0.5s ease-in-out'
+  
+    const menuDesktop1 = document.querySelector('#menu-desktop')
+    menuDesktop1.style.display = 'none'
+  
+    const setasCarrossel = document.querySelector('.swiper-button-prev')
+    setasCarrossel.style.display = 'none'
+  
+    const setasCarrossel2 = document.querySelector('.swiper-button-next')
+    setasCarrossel2.style.display = 'none'
+    
+    //Encostar o carrossel ao sidebar:
+    const carrossel = document.querySelector('.container-notices')
+    carrossel.style.marginLeft = '-50px'
+  
+  
+  }
+  function closeNav() {
+    document.getElementById("side_nav").style.width = "0";
+    // document.getElementById("#").style.marginLeft = "#";
+    
+    const body = document.querySelector('body')
+    body.style.marginLeft = '0'
+    body.style.transition = 'all 0.5s ease-in-out'
+  
+    const navbar = document.querySelector('.navbar-header')
+    navbar.style.transition = 'all 0.5s ease'
+  
+    const menuDesktop1 = document.querySelector('#menu-desktop')
+    menuDesktop1.style.display = 'block'
+  
+    const setasCarrossel = document.querySelector('.swiper-button-prev')
+    setasCarrossel.style.display = 'block'
+  
+    const setasCarrossel2 = document.querySelector('.swiper-button-next')
+    setasCarrossel2.style.display = 'block'
+    
+    //Voltar o carrossel para posição padrão, após fechar o sidebar
+    const carrossel = document.querySelector('.container-notices')
+    carrossel.style.marginLeft = '0px'
+  }
--- javascript/tecnologia/tecnologia-page-script.js
+++ javascript/tecnologia/tecnologia-page-script.js
@@ -203,6 +203,8 @@ carrosselScript.className = 'card swiper-slide'
     /*console.log(selecionarClasseCarrossel.innerText)*/
 }
 
+
+//Barra de pesquisa inicial (desativada)
 /*
 let x = document.getElementsByClassName('card swiper-slide');
     
@@ -229,6 +231,8 @@ function pesquisar() {
 }*/
 
 
+
+
 // Função para buscar os resultados na array
 function pesquisar(query) {
     // Converte a palavra digitada para letras minúsculas
@@ -246,11 +250,7 @@ function pesquisar(query) {
     // Adiciona o objeto à array de resultados
     results.push(arrayCarrossel[i]);
     }
-    }/*
-    // Ordena os resultados em ordem alfabética pelo nome
-    results.sort(function(a, b) {
-    return a.name.localeCompare(b.name);
-    });*/
+    }
     // Retorna a array de resultados
     return results;
     }
@@ -271,9 +271,9 @@ function showResults(results) {
     resultDiv.className = "search-result";
 
     var resultName = document.createElement('div')
-    resultName.classList.add('card')
+    resultName.classList.add('card23')
 
-    //Selecionando a div que mostrará os resultados na tela, e adicionando estilos para os conteúdos se ajustarem perfeitamente na tela.
+    //Selecionando a div que mostrará os resultados na tela, e adicionando estilos para os conteúdos se ajustarem melhor na tela.
     var resultadosMostradosNaTela = document.querySelector('.search-results')
     resultadosMostradosNaTela.style.display = 'flex'
     resultadosMostradosNaTela.style.width = 'auto'
@@ -282,53 +282,110 @@ function showResults(results) {
     resultadosMostradosNaTela.style.marginBottom = '10%'
 
     resultName.innerHTML = `
-    <a class="link-notices" target="_blank" href="${results[i].linkNotices}">
-        <div class="image-content">
-            <span class="overlay">
-                <div class="card-image">
-                    <img class="card-img" src="${results[i].cardImg}" alt="${results[i].altImgCard}">
-                </div>
-            </span>
-        </div>
-        <div class="card-content">
-            <h3 class="tittle_card">
-                ${results[i].tittleCard}
-            </h3>
-            <p class="description_card">
-                ${results[i].descriptionCard}
-            </p>
-        </div>
-    </a>
-
+    <div class = 'card'>
+        <a class="link-notices" target="_blank" href="${results[i].linkNotices}">
+            <div class="image-content">
+                <span class="overlay">
+                    <div class="card-image">
+                        <img class="card-img" src="${results[i].cardImg}" alt="${results[i].altImgCard}">
+                    </div>
+                </span>
+            </div>
+            <div class="card-content">
+                <h3 class="tittle_card">
+                    ${results[i].tittleCard}
+                </h3>
+                <p class="description_card">
+                    ${results[i].descriptionCard}
+                </p>
+            </div>
+        </a>
+    </div>
 `
     // Adiciona os elementos ao div do resultado
     resultDiv.appendChild(resultName);
 
     // Adiciona o div do resultado ao div dos resultados
     resultsDiv.appendChild(resultDiv);
+
+    //reativando as páginas do site
+    let htmlCarrossel = document.querySelector('.container-notices')
+    let adsSection = document.querySelector('.ads-section')
+    let containerNoticesPosAds = document.querySelector('.container_notices_pos_ads')
+    let botaoCarregarMais = document.querySelector('.btn-ver-mais')
+
+    htmlCarrossel.classList.remove ('desativado')
+    adsSection.classList.remove ('desativado')
+    containerNoticesPosAds.classList.remove ('desativado')
+    botaoCarregarMais.classList.remove ('desativado')
     }
-    } else {
+    }else {
     // Cria um elemento p para informar que não há resultados
-    var noResults = document.createElement("p");
-    noResults.textContent = "Nenhum resultado encontrado.";
+    var noResults = document.createElement("div");
+    noResults.classList.add('no-results-messenger-group')
+   /* noResults.textContent = "Desculpe, não encontramos resultados para sua pesquisa.";*/
+    noResults.innerHTML = `
+    <img class = 'no-results-img' src="../assets/images/tech-pesquisa-nao-encontrada-robo.png" alt="Resultado de Pesquisa Não Encontrado.">
+
+    <div class = 'text-no-results'>
+        <p>
+            Desculpe, não conseguimos obter resultados para sua pesquisa.
+        </p>
+        <br>
+        <p>
+            Você pode tentar pesquisar novamente, ver as principais notícias desta página, ou navegar por nossos outros temas disponíveis.
+        </p>
+    </div>
+    `
     // Adiciona o elemento ao div dos resultados
     resultsDiv.appendChild(noResults);
+
+    //desativando as páginas do site
+    let htmlCarrossel = document.querySelector('.container-notices')
+    let adsSection = document.querySelector('.ads-section')
+    let containerNoticesPosAds = document.querySelector('.container_notices_pos_ads')
+    let botaoCarregarMais = document.querySelector('.btn-ver-mais')
+
+    htmlCarrossel.classList.add ('desativado')
+    adsSection.classList.add ('desativado')
+    containerNoticesPosAds.classList.add ('desativado')
+    botaoCarregarMais.classList.add ('desativado')
     }
-    }
+}
+
 
 // Seleciona o elemento da barra de pesquisa
 var searchInput = document.getElementById("search");
+
 // Seleciona o elemento do botão de busca
 var searchButton = document.getElementById("search-button");
 // Adiciona um evento de clique ao botão de busca
-searchButton.addEventListener("click", function() {
+searchInput.addEventListener("keyup", function() {
 // Obtém o valor da barra de pesquisa
 var query = searchInput.value;
 // Verifica se o valor não está vazio
+
 if (query) {
 // Busca os resultados na array de dados
 var results = pesquisar(query);
 // Mostra os resultados na página
 showResults(results);
+}
+
+else {
+    let resultsDiv = document.getElementById("search-results");
+    resultsDiv.style.display = 'none'
+
+
+    //Reativando o site caso o usuário exclua os dados do input selecionando-os e depois deletando de uma só vez
+    let htmlCarrossel = document.querySelector('.container-notices')
+    let adsSection = document.querySelector('.ads-section')
+    let containerNoticesPosAds = document.querySelector('.container_notices_pos_ads')
+    let botaoCarregarMais = document.querySelector('.btn-ver-mais')
+
+    htmlCarrossel.classList.remove ('desativado')
+    adsSection.classList.remove ('desativado')
+    containerNoticesPosAds.classList.remove ('desativado')
+    botaoCarregarMais.classList.remove ('desativado')
 }
 });
